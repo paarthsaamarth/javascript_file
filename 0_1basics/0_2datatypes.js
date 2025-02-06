a37bff643117187b784a43f8b0248a3259b9271e
NIKHIL KUMAR BHASKAR <143253768+paarthsaamarth@users.noreply.github.com>
2025-02-06 17:57:26 +0000

    datatypes done

---
+++ 0_1basics/0_2datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; /* treat all JS code as newer version => The purpose of "use strict" is to 
+indicate that the code should be executed in "strict mode".With strict mode, you can not, 
+for example, use undeclared variables. */
+
+let name = "nikhil" // string type data
+let age = 21 // number data type
+let isLoggedIn = true // boolean type data
+
+// Primitive Data Types:-
+// In JavaScript, a primitive value is a single value with no properties or methods.
+// JavaScript has 7 primitive data types:-
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , representation of no value.
+// undefined => mean no value is defined to a variable during declaration.
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

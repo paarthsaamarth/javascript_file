ff94b84f7e0a7edaa888f01944097c0fa87bb7c4
NIKHIL KUMAR BHASKAR <143253768+paarthsaamarth@users.noreply.github.com>
2025-02-06 17:43:31 +0000

    variables done

---
+++ 0_1basics/0_1variables.js
@@ -0,0 +1,13 @@
+const accountId = 124
+let accountEmail = "nik11@gmail.com"
+var accountPwd = "6078"
+accountCity = "gaya"
+let accountState;
+
+// accountId = 7
+accountEmail = "bhaskar20@gmail.com"
+accountCity = "Bhopal"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPwd,accountCity,accountState])

273276224299163586dd65f86b3f77ddaa4dd83d
NIKHIL KUMAR BHASKAR <143253768+paarthsaamarth@users.noreply.github.com>
2025-05-09 20:58:38 +0000

    reviseD --> Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,15 @@
 const accountId = 124
 let accountEmail = "nik11@gmail.com"
-var accountPwd = "6078"
-accountCity = "gaya"
-let accountState;
+var accountPwd = "6078" /* Prefer not use var --> because of issue in block issue and functional scope */
+accountCity = "gaya" //in this way variable can also be declared , but it is recomended to not use it like this --> #BAD PRACTISE
+let accountState;//Agar aap javaScript mein variable ko declare kar ke choor denge without intialize kiye, toh javaScript uski value ko UNDEFINED maan legi
 
-// accountId = 7
+// accountId = 7 //--> not allowed , we can't change variable having keyword const
+
 accountEmail = "bhaskar20@gmail.com"
 accountCity = "Bhopal"
 
-console.log(accountId);
+console.table([accountId,accountEmail,accountPwd,accountCity,accountState])/*inorder to not write console.log() 
+again and again to print the value of more than one variable, we can simply use the console.table([]) once and 
+put all variables inside square braces followed by comma which you want to output on console.*/
 
-console.table([accountId,accountEmail,accountPwd,accountCity,accountState])

2c61da49fd8eca9192a8bdcf554b46ce28d3581e
NIKHIL KUMAR BHASKAR <143253768+paarthsaamarth@users.noreply.github.com>
2025-05-10 21:10:02 +0000

    reviseD --> dataTypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,6 +2,8 @@
 indicate that the code should be executed in "strict mode".With strict mode, you can not, 
 for example, use undeclared variables. */
 
+// alert( 2 + 3 ) //this will throw an error because we are using nodejs, not browser
+
 let name = "nikhil" // string type data
 let age = 21 // number data type
 let isLoggedIn = true // boolean type data
@@ -10,16 +12,17 @@ let isLoggedIn = true // boolean type data
 // In JavaScript, a primitive value is a single value with no properties or methods.
 // JavaScript has 7 primitive data types:-
 
-// number => 2 to power 53
+// number => 2 to power 53(range of number)
 // bigint
 // string => ""
 // boolean => true/false
-// null => standalone value , representation of no value.
-// undefined => mean no value is defined to a variable during declaration.
+// null => standalone value , representation of no value --> means khali hai.
+// undefined => mean no value is defined or Assigned to a variable during declaration.
 // symbol => unique
 
 
 // object
 
-console.log(typeof undefined); // undefined
+console.log(typeof "Nikhil"); // string
+console.log(typeof undefined); // undefined --> kyunki woh(undefined) ek type hai
 console.log(typeof null); // object

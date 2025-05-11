b5dad4a7bb7d2a6b27726a1c9125cf53331647e4
NIKHIL KUMAR BHASKAR <143253768+paarthsaamarth@users.noreply.github.com>
2025-05-11 21:23:12 +0000

    reviseD --> memoryConceptInJs

--- 01_basics/memorywork_in_js.js
+++ 01_basics/memorywork_in_js.js
@@ -8,15 +8,18 @@ anotherName = "nikhil"
 console.log(myName);
 console.log(anotherName);
 
+
+
+
 let userOne = {
     email: "benoverse11@gmail.com",
     upi : "2384892@ybl"
 }
 
-let userTwo = userOne
+let userTwo = userOne //userTwo got the reference of userOne not the copy of userOne
 
-userTwo.email = "nikhilbhaskar20@gmail.com"
+userTwo.email = "nikhilbhaskar20@gmail.com" //this changes will also reflect in the email of userOne because both userOne && userTwo has the same reference of the object(containing email and upi)
 
-console.log(userOne.email);
-console.log(userTwo.email);
+console.log(userOne.email);//nikhilbhaskar20@gmail.com  NOT  benoverse11@gmail.com
+console.log(userTwo.email);//nikhilbhaskar20@gmail.com
 

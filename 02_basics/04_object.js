9f7ef4bd3b3cf6a0bb52e9fe06290c111f5037e4
NIKHIL KUMAR BHASKAR <143253768+paarthsaamarth@users.noreply.github.com>
2025-05-22 13:06:16 +0000

    JSON talk

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -18,17 +18,40 @@ const regularUser = {
     }
 }
 
+//accessing the nested properties of an object
 // console.log(regularUser.fullname.userfullname.firstname); //accessing nested properties
+
+
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+/*Objects can be merged using Object.assign() or the spread syntax { ...obj1, ...obj2 }.*/
+// const obj3 = { obj1, obj2 } //it does not merge obj1 and obj2 into a single object — it nests them under their own keys.
+// const obj3 = Object.assign({}, obj1, obj2, obj4)//Object.assign(target, ...sources); --> The Object.assign() method is used to copy the values and properties from one or more source objects to a target object.
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2}//You are unpacking (spreading) all key-value pairs from obj1 and obj2 directly into obj3.The result is a flat object that contains properties from both.
 // console.log(obj3);
+
+/*
+
+✅ Final Summary:
+
+const obj1 = {
+    name : "Alice"
+}
+
+const obj2 = {
+    age : 25
+}
+
+SYNTAX	             |    RESULT TYPE     |    EXAMPLE OUTPUT :-
+                     |                    |
+{ obj1, obj2 }	     |    Nested object	  |    obj3 = { obj1: {...}, obj2: {...} }
+{ ...obj1, ...obj2 } |	  Merged object	  |    obj3 = { name: "Alice", age: 25 }
+
+*/
 
 
 //array of objects -> mostly used when data comes from database.
@@ -50,9 +73,9 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));//O/P ka datatype Array hai
-// console.log(Object.values(tinderUser));//O/P ka datatype Array hai
-// console.log(Object.entries(tinderUser));//O/P -> har ek key value ko ek array mein convert kiya jaata , fir un sabki array of key value pair ka bhi array bana ke output mein show hota hai
+console.log(Object.keys(tinderUser));//[ 'id', 'name', 'isLoggedIn' ] --> O/P ka datatype Array hai
+console.log(Object.values(tinderUser));//[ '123abc', 'Sammy', false ] --> O/P ka datatype Array hai
+console.log(Object.entries(tinderUser));//[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ] --> har ek key value ko ek array mein convert kiya jaata , fir un sabki array of key value pair ka bhi array bana ke output mein show hota hai
 
 
 //sometimes looping through object and you try to access one of the property of object which do not exist , can cause crash.
